8e7ddba88e8dd40e5e45fbaadb3f5c55023cdc5e
Ihar Shauchuk <shauchuk.ihar@gmail.com>
2022-05-11 11:19:58 +0300

    initial: created an api for a beauty salon

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Local Applications
+    'api.apps.ApiConfig',
+
+    # Third Party
+    'phonenumber_field',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -104,9 +111,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
