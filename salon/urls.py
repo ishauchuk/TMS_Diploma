0cdb801215aeb265ed0a0086fd65fdfad8fb43c6
Ihar Shauchuk <shauchuk.ihar@gmail.com>
2022-05-20 17:20:22 +0300

    feat: added templates

--- salon/urls.py
+++ salon/urls.py
@@ -3,9 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.home),
     path('about/', views.about),
     path('portfolio/', views.portfolio),
     path('price-list/', views.prices),
     path('order/', views.order),
+    path('masters/', views.masters),
 ]
--- salon/views.py
+++ salon/views.py
@@ -1,24 +1,47 @@
 from django.http import HttpResponse, HttpResponseNotFound
+from django.shortcuts import render
 
 
-def index(request):
-    return HttpResponse("Домашняя страница салона-красоты.")
+def home(request):
+    return render(request, 'home_page.html', context={
+        'title': 'Домашняя',
+        'description': 'Сайт салона-парикмехерской "Люби-себя"'
+    })
 
 
 def about(request):
-    return HttpResponse('Салон-парикмахерская "Люби Себя" рад видеть Вас.')
+    return render(request, 'home_page.html', context={
+        'title': 'О нас',
+        'description': 'Контактный номер телефона/Адрес'
+    })
 
 
 def prices(request):
-    return HttpResponse("Здесь находится список услуг и цены.")
+    return render(request, 'home_page.html', context={
+        'title': 'Прейскурант цен и услуг',
+        'description': 'Здесь находится список услуг с ценами'
+    })
 
 
 def portfolio(request):
-    return HttpResponse("Здесь находятся примеры работ наших мастеров.")
+    return render(request, 'home_page.html', context={
+        'title': 'Примеры работ',
+        'description': 'Здесь находятся фотографии работ наших мастеров'
+    })
 
 
 def order(request):
-    return HttpResponse("Здесь происходит запись на услуги")
+    return render(request, 'home_page.html', context={
+        'title': 'Запись на услуги',
+        'description': 'Здесь можно осуществить запись на услуги'
+    })
+
+
+def masters(request):
+    return render(request, 'home_page.html', context={
+        'title': 'Наши мастера',
+        'description': 'Здесь находится информация о наших мастерах'
+    })
 
 
 def pageNotFound(request, exception):
